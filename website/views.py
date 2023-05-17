c5d1bb9543485be2c462a05845aa88d1b8a72249
ahmadka00 <cl.ahmadka@gmail.com>
2023-05-17 16:00:39 +0200

    adding new fetures

--- website/views.py
+++ website/views.py
@@ -1,17 +1,62 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
+from .models import Record
 
 
 def home(request):
+    records = Record.objects.all()
+
+
+
     # Check to see of logging in
     if request.method == "POST":
         username = request.POST["username"]
         password = request.POST["password"]
-
-        
-    return render(request, 'website/home.html', {})
+        #authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You Have Been Logged In! ")
+            return redirect("home")
+        else:
+            messages.error(request,'There Was An Error Logging In, Please Try Again...')
+            return redirect('home')
+    else:
+        return render(request, 'website/home.html', {'records':records})
 
 
 def logout_user(request):
-    pass
+    logout(request)
+    messages.success(request, "You Have Been Logged Out ..")
+    return redirect('home')
+
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Authenticate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You Have Successfully Register")
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'website/register.html', {'form':form})
+
+    return render(request, 'website/register.html', {'form':form})
+
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        # Look Up Records
+        customer_record = Record.objects.get(id=pk)
+        return render(request, 'website/record.html', {'customer_record':customer_record})
+    else:
+        messages.error(request,"You Must Be Logged In To View That Page")
+        return redirect('home')
